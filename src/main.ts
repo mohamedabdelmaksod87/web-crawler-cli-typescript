bacae6c65ab9ac037a8f22952e4014da82ed2302
mohamedabdelmaksod87 <mohamedabdelmaksod1987@gmail.com>
2023-04-06 10:07:16 +0200

    change input from args to readline

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { crawlUrl } from "./crawl";
 import { printReport } from "./report";
+import * as readline from "readline/promises";
+import { stdin as input, stdout as output } from "process";
 
 async function main(): Promise<void> {
-  if (process.argv.length < 3) {
-    return console.log("No URL Provided");
+  while (true) {
+    const input = await getUserInput();
+    if (!input) continue;
+    if (input === "exit") break;
+    console.log(`Starting Crawl of ${input}`);
+    const pages = await crawlUrl(input, input, {});
+    printReport(pages);
   }
+}
 
-  if (process.argv.length > 3) {
-    return console.log("Too many command line args");
-  }
-
-  const baseUrl = process.argv[2];
-  console.log(`Starting Crawl of ${baseUrl}`);
-  const pages = await crawlUrl(baseUrl, baseUrl, {});
-  printReport(pages);
+async function getUserInput(): Promise<string> {
+  const rl = readline.createInterface({ input, output });
+  const answer = await rl.question("Enter a valid URL or exit to close:\n>");
+  rl.close();
+  return answer.trim();
 }
 
 main();
